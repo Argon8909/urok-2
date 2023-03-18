8e88ca198cf970e3ce230739fc6cc41f7bd4c19a
Arkadiy Goncharov <argon1989@ya.ru>
2023-03-19 00:07:16 +0300

    изменил строку

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("новая ветка!");
             Console.WriteLine("Hello World33!");
             Console.WriteLine("Hello World22!");
+            Console.WriteLine("новая ветка-2!");
         }
     }
 }
