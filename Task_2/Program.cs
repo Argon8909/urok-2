b5b28034c39c142bceba2a8a910ec4ecf038b429
Arkadiy Goncharov <argon1989@ya.ru>
2023-03-22 01:23:43 +0300

    дз-3

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,4 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Main();
 
-Console.WriteLine("новая ветка!");
-Console.WriteLine("новая ветка!");
+void Main()
+{
+    Console.WriteLine("Введите первое число");
+    double input1 = ParseOrRerun(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    double input2 = ParseOrRerun(Console.ReadLine());
+
+    Console.WriteLine("Результат:");
+    Console.WriteLine($"Сложение:  {input1} + {input2} = {input1 + input2}");
+    Console.WriteLine($"Вычитание: {input1} - {input2} = {input1 - input2}");
+    Console.WriteLine($"Умножение: {input1} * {input2} = {input1 * input2}");
+    Console.WriteLine($"Деление:   {input1} / {input2} = {input1 / input2}");
+}
+
+
+double ParseOrRerun(string? inputValue)
+{
+    double result;
+    var parsed = double.TryParse(inputValue, out result);
+
+    if (!parsed)
+    {
+        Console.WriteLine("Ошибка ввода! Введите заново");
+        Main();
+    }
+
+    return result;
+}
