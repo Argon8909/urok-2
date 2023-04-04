7c3a5128ba59f822813816786cf39ceadbe2f3ab
Arkadiy Goncharov <argon1989@ya.ru>
2023-04-04 18:53:22 +0300

    на проверку

---
+++ Class Car/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Class_Car;
+using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Car car = new Car(2010, Car.CarColor.Black, 10000);
+
+
+        Console.WriteLine(car.GetCarYear());
+        Console.WriteLine(car.GetMileage());
+
+        Console.WriteLine($"1. {car}");
+
+        car.GoOneMile();
+        car.GoOneMile();
+        car.GoOneMile();
+
+        Console.WriteLine(car.GetMileage());
+
+        Console.WriteLine(car.GetCarYear());
+
+        Console.WriteLine($"2. {car}");
+
+
+
+    }
+
+}
---
+++ Fibb/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine(ReturnTuple(10));
+Console.WriteLine(Fibonacci(10));
+
+
+int Fibonacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+(int, int) ReturnTuple(int n)
+{
+    int x = n * n;
+    int y = n + n;
+    
+    return (x, y);
+}
