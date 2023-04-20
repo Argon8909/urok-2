86fd0bd573595ff5ddd24ad87a061114a3e167ba
Arkadiy Goncharov <argon1989@ya.ru>
2023-04-20 21:47:16 +0300

    Восстановил потерявшийся файл Program.cs

---
+++ Пылесосы дз-8/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HW_8;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cleansArray = new VacuumCleaner[]
+            {
+                new DustBag("Samsung", 9),
+                new Robot("Xiaomi", 22),
+                new Washing("Kerher", 45),
+            };
+            int i = 0;
+            foreach (var vacuumCleaner in cleansArray)
+            {
+
+                if (vacuumCleaner is Washing)
+                {
+                    ((Washing) vacuumCleaner).StartCleaning();
+                }
+                else
+                {
+                    vacuumCleaner.StartCleaning();
+                }
+              
+
+            }
+
+        }
+    }
