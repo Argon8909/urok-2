f31a3d1b812c0e8fca4b7206fb8a415407bdcc45
Arkadiy Goncharov <argon1989@ya.ru>
2023-04-23 03:32:04 +0300

    start

---
+++ ДЗ - 11 - 2/InputParse.cs
@@ -0,0 +1,18 @@
+﻿namespace ДЗ___11___2;
+
+
+public static class InputParse
+{
+    private static int ParseStringToInt(string inputValue)
+    {
+        int result;
+        var parsed = int.TryParse(inputValue, out result);
+
+        if (!parsed)
+        {
+            Console.WriteLine("Wrong input. Try again please");
+        }
+
+        return result;
+    }
+}
---
+++ ДЗ-11/InputParse.cs
@@ -0,0 +1,17 @@
+﻿namespace ДЗ_11;
+
+public static class InputParse
+{
+    private static int ParseStringToInt(string inputValue)
+    {
+        int result;
+        var parsed = int.TryParse(inputValue, out result);
+
+        if (!parsed)
+        {
+            Console.WriteLine("Wrong input. Try again please");
+        }
+
+        return result;
+    }
+}
