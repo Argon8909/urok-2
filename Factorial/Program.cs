17c2eed736f0607ab885ee30a32f980748788413
Arkadiy Goncharov <argon1989@ya.ru>
2023-06-07 14:19:40 +0300

    Start

---
+++ Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Factorial;
+
+static class Program
+{
+    // static Queue<PublicTransport> _routeToTheOffice = new();
+    //static Stack<PublicTransport> _routeHome = new();
+
+    private const int ArrCapacity = 10;
+    private static List<int> Arr = new();
+    private static Random _random = new();
+
+    public static void Main()
+    {
+        for (int i = 0; i < ArrCapacity; i++)
+        {
+            int q = _random.Next(0, 999);
+            Arr.Add(q);
+        }
+
+        foreach (var VARIABLE in Arr)
+        {
+            Console.Write(VARIABLE + ", ");
+        }
+    }
+}
