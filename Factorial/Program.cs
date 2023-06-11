e6cc3d91a1ef3c9bbec91164c47e47677eccef8e
Arkadiy Goncharov <argon1989@ya.ru>
2023-06-11 21:57:22 +0300

    в работе

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,104 +1,8 @@
-﻿/*
-namespace Factorial
-{
-    static class Program
-    {
-        private const int ArrCapacity = 10;
-        private static List<int> Arr = new();
-        private static Random _random = new();
-
-        public static async Task Main()
-        {
-            for (int i = 0; i < ArrCapacity; i++)
-            {
-                int q = _random.Next(0, 999);
-                Arr.Add(q);
-            }
-
-            foreach (var val in Arr)
-            {
-                Console.WriteLine(await CalculateFactorialAsync(val));
-            }
-        }
-
-        public static async Task<int> CalculateFactorialAsync(int n)
-        {
-            if (n < 0)
-                throw new ArgumentException("Входные данные должны быть неотрицательным целым числом.", nameof(n));
-
-            if (n == 0 || n == 1)
-                return 1;
-
-            int factorial = 1;
-
-            for (int i = 2; i <= n; i++)
-            {
-                factorial *= i;
-            }
-
-            return factorial;
-        }
-    }
-}
-
-
-
-
-
-namespace Factorial
-{
-    static class Program
-    {
-        private const int ArrCapacity = 10;
-        private static List<int> Arr = new();
-        private static Random _random = new();
-
-        public static void Main()
-        {
-            List<Task> tasks = new List<Task>();
-
-            for (int i = 0; i < ArrCapacity; i++)
-            {
-                int q = _random.Next(0, 999);
-                Arr.Add(q);
-            }
-
-            foreach (var val in Arr)
-            {
-                tasks.Add(Task.Run(() => Console.WriteLine(CalculateFactorialAsync(val))));
-            }
-
-            Task.WaitAll(tasks.ToArray());
-        }
-
-        public static async Task<int> CalculateFactorialAsync(int n)
-        {
-            if (n < 0)
-                throw new ArgumentException("Входные данные должны быть неотрицательным целым числом.", nameof(n));
-
-            if (n == 0 || n == 1)
-                return 1;
-
-            int factorial = 1;
-            await Task.Yield();
-
-            for (int i = 2; i <= n; i++)
-            {
-                factorial *= i;
-            }
-
-            return factorial;
-        }
-    }
-}
-*/
-
-namespace Factorial;
+﻿namespace Factorial;
 
 static class Program
 {
-    // static Queue<PublicTransport> _routeToTheOffice = new();
-    //static Stack<PublicTransport> _routeHome = new();
+    private static List<Task> _tasks = new List<Task>();
 
     private const int ArrCapacity = 10;
     private static List<int> Arr = new();
@@ -106,67 +10,43 @@ static class Program
 
     public static void Main()
     {
-        List<Task> tasks = new List<Task>();
-
+       // List<Task> tasks = new List<Task>();
+       _tasks.Add(() => new Task());
+       
+       
         for (int i = 0; i < ArrCapacity; i++)
         {
             int q = _random.Next(0, 999);
             Arr.Add(q);
         }
-        
-        foreach (var VARIABLE in Arr)
+
+        foreach (var integer in Arr)
         {
-            Console.Write(VARIABLE + ", ");
+            Console.Write(integer + ", ");
         }
 
+
+/*
         foreach (var val in Arr)
         {
             tasks.Add(Task.Run(() => Console.WriteLine(CalculateFactorialAsync(val))));
             //tasks.Add(new Task(() => Console.WriteLine(CalculateFactorialAsync(val))));
         }
+*/
 
+
+        // Task.WaitAll(tasks.ToArray());
+    }
+
+    static void TasksdDistributor(List<int> arr)
+    {
+        Queue<int> queueIntegerToFactorial = new();
+        foreach (var integer in arr)
+        {
+           queueIntegerToFactorial.Enqueue(integer); 
+        }
         
-
-        Task.WaitAll(tasks.ToArray());
-    }
-
-    public static async Task<int> CalculateFactorialAsync(int n)
-    {
-        if (n < 0)
-            throw new ArgumentException("Входные данные должны быть неотрицательным целым числом.", nameof(n));
-
-        if (n == 0 || n == 1)
-            return 1;
-
-        int factorial = 1;
-        await Task.Yield(); // позволяет передать управление обратно вызывающему коду и продолжить выполнение асинхронно
-
-        for (int i = 2; i <= n; i++)
-        {
-            factorial *= i;
-        }
-
-        return factorial;
-    }
-
-
-    public static async Task<int> CalculateFactorialAsync1(int n)
-    {
-        if (n < 0)
-            throw new ArgumentException("The input must be a non-negative integer.", nameof(n));
-
-        if (n == 0 || n == 1)
-            return 1;
-
-        int factorial = 1;
-        for (int i = 2; i <= n; i++)
-        {
-            factorial *= i;
-            // добавляем небольшую задержку для демонстрации асинхронности
-            await Task.Delay(100);
-        }
-
-        return factorial;
+        
     }
 
     public static int CalculateFactorial(int n)
@@ -178,6 +58,7 @@ static class Program
             return 1;
 
         int factorial = 1;
+
         for (int i = 2; i <= n; i++)
         {
             factorial *= i;
