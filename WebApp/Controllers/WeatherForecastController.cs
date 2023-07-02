6d6198b10ce1fbf1952df72d6fb8b8d1eeeb037a
Arkadiy Goncharov <argon1989@ya.ru>
2023-07-03 00:43:12 +0300

    разобрался с адресацией

---
+++ WebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("api/")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+
+    public WeatherForecastController()
+    {
+    }
+
+    [HttpGet]
+    [Route("GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var weatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        return weatherForecast;
+    }
+
+    [HttpGet]
+    [Route("GetText")]
+    public string Get2()
+    {
+        return "text.ToUpper()";
+    }
+}
--- WebApp/Item.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApp;
-
-public class Item
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
