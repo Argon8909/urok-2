9ec1bfabdc938db8dad71f7d133097a244db72fc
Arkadiy Goncharov <argon1989@ya.ru>
2023-07-05 20:49:16 +0300

    1

---
+++ WebApp/Controllers/PhoneBookController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class PhoneBook : Controller
+{
+    private List<PhoneRecord> _book = new List<PhoneRecord>();
+    
+   
+    [HttpGet]
+    [Route("GetPhoneBook")]
+    public string GetAll()
+    { 
+        string? str = _book.Select(x => x.Name).ToString();
+        return str;
+    }
+}
---
+++ WebApp/PhoneRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp;
+
+public class PhoneRecord
+{
+    public readonly string Name;
+    public readonly string Number;
+    public readonly string? Adress;
+
+    public PhoneRecord(string name, string number, string? adress = null)
+    {
+        Name = name;
+        Number = number;
+        Adress = adress;
+    }
+}
