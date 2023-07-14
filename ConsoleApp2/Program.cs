b59ef9803bf9844e852dfd6cb1ae20696318dfef
Arkadiy Goncharov <argon1989@ya.ru>
2023-07-14 10:28:10 +0300

    на проверку

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        var httpClientHandler = new HttpClientHandler();
+        httpClientHandler.ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true;
+
+        var httpClient = new HttpClient(httpClientHandler);
+        var baseAddress = "http://localhost:5137/api/PhoneBook";
+
+        // Вызов метода GetAll() - HTTP GET
+        var getAllResponse = await httpClient.GetAsync($"{baseAddress}/GetAllPhoneBook");
+        var getAllResult = await getAllResponse.Content.ReadAsStringAsync();
+        Console.WriteLine("Get>>> " + getAllResult);
+
+
+        // Вызов метода PostContact() - HTTP POST
+        NameValueCollection queryStringPost = System.Web.HttpUtility.ParseQueryString(string.Empty);
+        queryStringPost.Add("name", "John");
+        queryStringPost.Add("number", "1234567890");
+        queryStringPost.Add("adress", "Moskow");
+
+        var postResponse = await httpClient.PostAsync($"{baseAddress}/PostPhoneBook?{queryStringPost}", null);
+        var postResult = await postResponse.Content.ReadAsStringAsync();
+        Console.WriteLine("Post>>> " + postResult);
+        Console.WriteLine(postResponse.RequestMessage.RequestUri.AbsoluteUri);
+
+
+        // Вызов метода PatchContact() - HTTP PATCH
+        NameValueCollection queryStringPatch = System.Web.HttpUtility.ParseQueryString(string.Empty);
+        queryStringPatch.Add("name", "John");
+        queryStringPatch.Add("newName", "Jane");
+        var patchResponse = await httpClient.PatchAsync($"{baseAddress}/PatchPhoneBook?{queryStringPatch}", null);
+        var patchResult = await patchResponse.Content.ReadAsStringAsync();
+        Console.WriteLine("Patch>>> " + patchResult);
+        Console.WriteLine(patchResponse.RequestMessage.RequestUri.AbsoluteUri);
+
+
+        // Вызов метода PutContact() - HTTP PUT
+        NameValueCollection queryStringPut = System.Web.HttpUtility.ParseQueryString(string.Empty);
+        queryStringPut.Add("name", "Jane");
+        queryStringPut.Add("newName", "Mike");
+        queryStringPut.Add("newNumber", "9876543210");
+        queryStringPut.Add("newAdress", "123 Main St");
+        
+        var putResponse = await httpClient.PutAsync($"{baseAddress}/PutPhoneBook?{queryStringPut}", null);
+        //var putResponse = await httpClient.PutAsync($"{baseAddress}/PutPhoneBook", putContent);
+        var putResult = await putResponse.Content.ReadAsStringAsync();
+        Console.WriteLine("Put>>> " + putResult);
+        Console.WriteLine(putResponse.RequestMessage.RequestUri.AbsoluteUri);
+
+
+        // Вызов метода DeleteContact() - HTTP DELETE
+        var deleteResponse = await httpClient.DeleteAsync($"{baseAddress}/DeletePhoneBook?name=Mike");
+        var deleteResult = await deleteResponse.Content.ReadAsStringAsync();
+        Console.WriteLine("Delete>>> " + deleteResult);
+    }
+}
+
+
+/*
+
+
+*/
--- WebApp/Controllers/PhoneBookController.cs
+++ WebApp/Controllers/PhoneBookController.cs
@@ -19,8 +19,10 @@ public class PhoneBook : Controller
     [Route("GetAllPhoneBook")]
     public string GetAll()
     {
-        if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
+        //if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
         // StringBuilder sb = new StringBuilder($"Всего записей {_book.Count} ");
+        CheckingForRepeat();
+
         var str = _book
             .Select(x => x
                 .Name + " " + x
@@ -36,7 +38,7 @@ public class PhoneBook : Controller
     [Route("GetContact")]
     public string GetContact(string name)
     {
-        if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
+        CheckingForRepeat();
 
         for (int i = 0; i < _book.Count; i++)
         {
@@ -56,10 +58,10 @@ public class PhoneBook : Controller
 
     [HttpPost]
     [Route("PostPhoneBook")]
-    public string PostContact(string name, string number, string? adress = null)
+    public string PostContact(string name, string number, string adress = "none")
     {
-        if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
-
+        //if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
+        CheckingForRepeat();
         PhoneRecord record = new PhoneRecord(name.Trim(), number.Trim(), adress.Trim());
         _book.Add(record);
         FileSaver.SaveBookToFile(_book);
@@ -70,8 +72,7 @@ public class PhoneBook : Controller
     [Route("DeletePhoneBook")]
     public string DeleteContact(string name)
     {
-        if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
-
+        CheckingForRepeat();
         for (int i = 0; i < _book.Count; i++)
         {
             if (_book[i].Name == name.Trim())
@@ -88,46 +89,78 @@ public class PhoneBook : Controller
 
     [HttpPatch]
     [Route("PatchPhoneBook")]
-    public string PatchContact(string name, string? mewName, string? newNumber, string? newAdress)
+    public string PatchContact(string name, string? newName, string? newNumber, string? newAdress)
     {
-        if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
-
-        for (int i = 0; i < _book.Count; i++)
+        CheckingForRepeat();
+        if (_book != null)
         {
-            if (_book[i].Name == name.Trim())
+            var q = _book.Where(t => t.Name == name.Trim());
+            foreach (var t in q)
             {
-                if (mewName != null) _book[i].Name = mewName.Trim();
-                if (newNumber != null) _book[i].Number = newNumber.Trim();
-                if (newAdress != null) _book[i].Adress = newAdress.Trim();
+                if (newName != null) t.Name = newName.Trim();
+                if (newNumber != null) t.Number = newNumber.Trim();
+                if (newAdress != null) t.Adress = newAdress.Trim();
                 FileSaver.SaveBookToFile(_book);
-                Console.WriteLine(name + " заменён на " + mewName);
-                return name + " заменён на " + mewName;
+                Console.WriteLine(name + " изменён ");
+                return name + " " + Param();
             }
         }
 
         return name + " is not found :-(";
+
+        string Param()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (newName != null) sb.Append("изменено имя, ");
+            if (newNumber != null) sb.Append("изменён номер, ");
+            if (newAdress != null) sb.Append("изменён адрес ");
+            return sb.ToString();
+        }
     }
 
 
     [HttpPut]
     [Route("PutPhoneBook")]
-    public string PutContact(string name, string mewName = "none", string newNumber = "none", string newAdress = "none")
+    public string PutContact(string name, string newName = "none", string newNumber = "none", string newAdress = "none")
     {
-        if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
-
-        for (int i = 0; i < _book.Count; i++)
-        {
-            if (_book[i].Name == name.Trim())
+        //if (_book != null && _book.Count == 0) _book = FileSaver.ReadBookFromFile();
+        CheckingForRepeat();
+        if (_book != null)
+            foreach (var t in _book.Where(t => t.Name == name.Trim()))
             {
-                _book[i].Name = mewName.Trim();
-                _book[i].Number = newNumber.Trim();
-                _book[i].Adress = newAdress.Trim();
+                t.Name = newName.Trim();
+                t.Number = newNumber.Trim();
+                t.Adress = newAdress.Trim();
                 FileSaver.SaveBookToFile(_book);
-                Console.WriteLine(name + " заменён на " + mewName);
-                return name + " заменён на " + mewName;
+                Console.WriteLine(name + " заменён на " + newName);
+                return name + " заменён на " + newName;
             }
-        }
 
         return name + " is not found :-(";
+    }
+
+    private void CheckingForRepeat()
+    {
+        if (_book.Count == 0) _book = FileSaver.ReadBookFromFile();
+        
+         //Если успею, то доделаю
+         
+        var hasDuplicates = _book
+            .GroupBy(x => x
+                .Name)
+            .Where(x => x
+                .Count() > 1)
+            .Select(x => x
+                .Select(x => x
+                    .Name = x
+                    .Name + " " + _random
+                    .Next(1, 100)));
+    }
+
+    private Random _random = new Random();
+
+    private string NameModificator(string name)
+    {
+        return name + " " + _random.Next(1, 100);
     }
 }
