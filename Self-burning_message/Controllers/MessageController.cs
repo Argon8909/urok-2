c37a92aea7f7c4e4200f1c8ef5c183475962710b
Arkadiy Goncharov <argon1989@ya.ru>
2023-07-25 20:59:36 +0300

    набросок сайта

---
+++ Self-burning_message/Controllers/MessageController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Self_burning_message.Models;
+
+
+namespace Self_burning_message.Controllers;
+
+[ApiController]
+[Route("api/messages")]
+public class MessageController : ControllerBase
+{
+    private static readonly List<Message> Messages = new List<Message>();
+
+    [HttpPost]
+    public IActionResult CreateMessage(Message message)
+    {
+        // Генерируем уникальную ссылку для сообщения
+        message.UniqueLink = Guid.NewGuid().ToString();
+     
+        // Добавляем сообщение в список
+        Messages.Add(message);
+
+        // Возвращаем уникальную ссылку
+        return Ok(new { Link = message.UniqueLink });
+    }
+
+    [HttpGet("{link}")]
+    public IActionResult GetMessage(string link)
+    {
+        // Находим сообщение по уникальной ссылке
+        var message = Messages.FirstOrDefault(m => m.UniqueLink == link);
+
+        if (message == null)
+        {
+            return NotFound();
+        }
+
+        // Если сообщение не прочитано, отмечаем его как прочитанное
+        if (!message.IsRead)
+        {
+            message.IsRead = true;
+        }
+
+        // Удаляем сообщение из списка (самоуничтожение)
+        Messages.Remove(message);
+
+        // Возвращаем содержимое сообщения
+        return Ok(new { Content = message.Content });
+    }
+}
---
+++ Self-burning_message/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Self_burning_message.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public bool IsRead { get; set; }
+    public string UniqueLink { get; set; }
+}
